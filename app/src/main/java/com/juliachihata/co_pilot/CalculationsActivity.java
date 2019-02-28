f67afa61df68fa0187d0e212caf3d83fcd109283
shuridbiswas <shurid_07@hotmail.com>
2019-02-27 23:47:34 -0500

    awareness xml
    Added activities xml for the  awareness test

--- app/src/main/java/com/juliachihata/co_pilot/CalculationsActivity.java
+++ app/src/main/java/com/juliachihata/co_pilot/CalculationsActivity.java
@@ -1,7 +1,10 @@
 package com.juliachihata.co_pilot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CalculationsActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class CalculationsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculations);
+
+        Button altcbutton = findViewById(R.id.altcorrection_button);
+
+
+        altcbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int1= new Intent(CalculationsActivity.this, AltcorrectionActivity.class);
+                startActivity(int1);
+            }
+
+
+        });
     }
 }
