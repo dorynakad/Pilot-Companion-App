dcb76f3c3e02c807b8fccfa6cc08289357184e6c
shuridbiswas <shurid_07@hotmail.com>
2019-02-27 23:47:34 -0500

    Push1
    Altitude Correction Activity

---
+++ app/src/main/java/com/juliachihata/co_pilot/AltcorrectionActivity.java
@@ -0,0 +1,13 @@
+package com.juliachihata.co_pilot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AltcorrectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_altcorrection);
+    }
+}
