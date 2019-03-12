630418954bca4c2ef9bb7f792682346321f2d298
Julia <juliachihata@gmail.com>
2019-03-12 19:20:15 -0400

    Icons and fonts

--- app/src/main/java/com/juliachihata/co_pilot/DictionaryActivity.java
+++ app/src/main/java/com/juliachihata/co_pilot/DictionaryActivity.java
@@ -9,5 +9,9 @@ public class DictionaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
+
+        getSupportActionBar().setTitle("Back");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
--- app/src/main/java/com/juliachihata/co_pilot/MissionActivity.java
+++ app/src/main/java/com/juliachihata/co_pilot/MissionActivity.java
@@ -9,5 +9,9 @@ public class MissionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mission);
+
+
+        getSupportActionBar().setTitle("Back");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
