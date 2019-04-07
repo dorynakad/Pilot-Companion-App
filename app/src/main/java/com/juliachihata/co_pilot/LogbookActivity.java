53d075d743f706408bfdeaf8c47b4c52b296e9ac
Julia C <juliachihata@gmail.com>
2019-04-07 01:43:45 -0400

    Fix the implementation of the logbook
    -Fix the implementation of the add and update functions.
    -Refactor/cleanup code.
    -Modify the xml
    -Add some resources

--- app/src/main/java/com/juliachihata/co_pilot/LogbookActivity.java
+++ app/src/main/java/com/juliachihata/co_pilot/LogbookActivity.java
@@ -1,35 +1,44 @@
 package com.juliachihata.co_pilot;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LogbookActivity extends AppCompatActivity {
 
     HashSet<String> emptySet;  //Creating an empty set so that the set would be "empty" and not "invalid" in case it is
     ListView lv;
-    EditText timeTxt;
-    EditText timeTxt2;
-    EditText DatePTxt;
+    TextView date_view;
+    EditText timeTxt, timeTxt2;
     Button  addBtn,updateBtn,clearBtn,deleteBtn;
     ArrayList<String> flightTimes;
     ArrayAdapter<String> adapter;
-    SharedPreferences sharedPreferences;static final String PREFS_NAME = "MyPrefsFile";
+    SharedPreferences sharedPreferences;
+
+    private int day, month, year;
 //    int white = android.R.color.white;
 
     //the two lines below should let the user input numbers from 1 to 12 , the code is in InputFilterMinMax
@@ -38,13 +47,11 @@ public class LogbookActivity extends AppCompatActivity {
 
 //et.setFilters(new InputFilter[]{ new InputFilterMinMax("1", "12")});
 
-//public
+    //public
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logbook);
-     //   SharedPreferences settings = getSharedPreferences(PREFS_NAME,0);
-     //   flightTimes = settings.getString("flightTimes",flightTimes);
 
         flightTimes = new ArrayList<String>();
         sharedPreferences = getSharedPreferences("mySharedPrefs", MODE_PRIVATE);
@@ -53,42 +60,57 @@ public class LogbookActivity extends AppCompatActivity {
 
         lv=findViewById(R.id.listView);
         timeTxt =  findViewById(R.id.timeTxt);
-        DatePTxt = findViewById(R.id.datePickerTxt);
-      //  timeTxt2 = findViewById(R.id.timeTxt2);
+        date_view = (TextView) findViewById(R.id.set_date);
+        //  timeTxt2 = findViewById(R.id.timeTxt2);
         addBtn  =  findViewById(R.id.addBtn);
         updateBtn   =   findViewById(R.id.updateBtn);
         clearBtn    =   findViewById(R.id.cleanBtn);
         deleteBtn   =   findViewById(R.id.deleteBtn);
 
         //Adapter
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, flightTimes){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                View view = super.getView(position, convertView, parent);
+                TextView ListItemShow = (TextView) view.findViewById(android.R.id.text1);
+                ListItemShow.setTextColor(Color.parseColor("white"));
+                return view;
+            }
+        };
 
-        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_single_choice , flightTimes);
         lv.setAdapter(adapter);
         //Set selected item
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View v, int pos, long id) {
 
-                timeTxt.setText(flightTimes.get(pos));
-
-
+                // This is to match the content of the input added/ to be updated
+                Pattern p_t = Pattern.compile("\\d(?= Hours)");
+                Pattern p_d = Pattern.compile("(?<=\\[).*?(?=\\])");
+                Matcher m_t = p_t.matcher(flightTimes.get(pos));
+                Matcher m_d = p_d.matcher(flightTimes.get(pos));
+                while (m_t.find()) {
+                    timeTxt.setText(m_t.group());
+                    break;
+                }
+                while (m_d.find()) {
+                    date_view.setText(m_d.group());
+                    break;
+                }
             }
         });
-    //Handle events
+
+        //Handle events
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 add();
-                saveEntries();
-
-
             }
         });
         updateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 update();
-                saveEntries();
             }
         });
 
@@ -96,7 +118,6 @@ public class LogbookActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 delete();
-                saveEntries();
             }
         });
         clearBtn.setOnClickListener(new View.OnClickListener() {
@@ -125,7 +146,6 @@ public class LogbookActivity extends AppCompatActivity {
 
         });
 
-
         emptySet = new HashSet<String>();
         emptySet.add("");
         loadEntries();
@@ -135,7 +155,6 @@ public class LogbookActivity extends AppCompatActivity {
     public String[] splitString(String s) {
         return s.split(" ");
     }
-
 
     public void saveEntries() { //So that we can save the logbook on the phone (Hopefully on the clouds later
         HashSet<String> set = new HashSet<String>(flightTimes);
@@ -161,32 +180,37 @@ public class LogbookActivity extends AppCompatActivity {
     }
 
     Date currentTime = Calendar.getInstance().getTime();
-    //Add
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE, MMMM dd, yyyy 'at' HH:mm:ss");
+    String cTime = simpleDateFormat.format(currentTime);
 
+    //Add
     private void add(){
 
-       String flightTime=timeTxt.getText().toString() ;//+ timeTxt2.getText().toString();
-        String dateLog = DatePTxt.getText().toString() ;
-
-      //  int timeInt = timeTxt.getString();
+        String flightTime=timeTxt.getText().toString();
+        String dateLog = date_view.getText().toString();
 
         if(!flightTime.isEmpty() && flightTime.length()>0){
-            //Add
-            if (Integer.parseInt(flightTime)< 0 || Integer.parseInt(flightTime)>5){
+            if (dateLog.isEmpty())
+                Toast.makeText(getApplicationContext(),"Please choose a date", Toast.LENGTH_LONG).show();
+            if (Integer.parseInt(flightTime) < 0 || Integer.parseInt(flightTime) > 5){
                 Toast.makeText(getApplicationContext(),"Flight time Invalid", Toast.LENGTH_LONG).show();
-
-                if (!dateLog.isEmpty())
-                        adapter.add(currentTime+ "\n" + dateLog + "    " + flightTime+" Hours");
-                return;
             }
-//            adapter.add(currentTime+"    "+flightTime+" Hours");
-            //Refresh
-            adapter.notifyDataSetChanged();
+            else {
+                //Add
+                if (!dateLog.isEmpty()) {
+                    adapter.add(cTime + "\n[" + dateLog + "]    " + flightTime + " Hours");
+                    saveEntries();
+                }
 
-            timeTxt.setText("");
+                //Refresh
+                adapter.notifyDataSetChanged();
 
-           Toast.makeText(getApplicationContext(),"Added "+flightTime +" hours" , Toast.LENGTH_SHORT).show();
+                //clear contexts
+                timeTxt.setText("");
+                date_view.setText("");
 
+                Toast.makeText(getApplicationContext(),"Successfully Added!" , Toast.LENGTH_SHORT).show();
+            }
         }
         else {
             Toast.makeText(getApplicationContext(),"Nothing to add", Toast.LENGTH_LONG).show();
@@ -194,59 +218,76 @@ public class LogbookActivity extends AppCompatActivity {
     }
 
     //Update
-
     private void update(){
         String flightTime=timeTxt.getText().toString();
+        String dateLog = date_view.getText().toString();
 
         //GET POS OF SELECTED ITEM
         int pos=lv.getCheckedItemPosition();
-        if(!flightTime.isEmpty() && flightTime.length()>0  ){
+
+        if (Integer.parseInt(flightTime) < 0 || Integer.parseInt(flightTime) > 5){
+            Toast.makeText(getApplicationContext(),"Flight time Invalid", Toast.LENGTH_LONG).show();
+        }
+        else {
+            String save_record = flightTimes.get(pos);
 
             //Remove item
             adapter.remove(flightTimes.get(pos));
 
-            //Insert
+            String new_record = save_record.replaceAll("\\d(?= Hours)", flightTime);
+            new_record = new_record.replaceAll("(?<=\\[).*?(?=\\])", dateLog);
 
-            adapter.insert(flightTime,pos);
+            //Insert
+            adapter.insert(new_record,pos);
 
             //refresh
             adapter.notifyDataSetChanged();
-            Toast.makeText(getApplicationContext(),"Updated" + timeTxt, Toast.LENGTH_SHORT).show();}
-            else {
-            Toast.makeText(getApplicationContext(), " Nothing to update" , Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(),"Successfully Updated!", Toast.LENGTH_SHORT).show();
+        }
+        if (flightTime.isEmpty()){
+            Toast.makeText(getApplicationContext(), "Nothing to update" , Toast.LENGTH_SHORT).show();
         }
         saveEntries();
+    }
 
-        
-
-        }
-
-        //delete
+    //delete
     private void delete(){
 
         int pos=lv.getCheckedItemPosition();
         if(pos > -1)
         {
-                //remove
+            //remove
             adapter.remove(flightTimes.get(pos));
             //refresh
             adapter.notifyDataSetChanged();
 
             timeTxt.setText("");
-            Toast.makeText(getApplicationContext(), "Delete" , Toast.LENGTH_SHORT).show();}
-            else{
+            Toast.makeText(getApplicationContext(), "Successfully Deleted!" , Toast.LENGTH_SHORT).show();}
+        else{
             Toast.makeText(getApplicationContext(), "Nothing to delete" , Toast.LENGTH_SHORT).show();
 
         }
-
-        
-
     }
 
     //clear
     private void clear(){
-    adapter.clear();
-    saveEntries();
+        adapter.clear();
+        saveEntries();
+    }
+
+    public void setDate(View view) {
+        final Calendar calendar = Calendar.getInstance();
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        month = calendar.get(Calendar.MONTH);
+        year = calendar.get(Calendar.YEAR);
+        DatePickerDialog dpd = new DatePickerDialog(LogbookActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int y, int m, int d) {
+                date_view.setText(m + " / " + d + ", " + y);
+            }
+        }, day, month, year);
+        dpd.getDatePicker().setMinDate(System.currentTimeMillis());
+        dpd.show();
     }
 
 }
