2c701411b2f7e5411f369e6d612638e4399520a2
Julia C <juliachihata@gmail.com>
2019-04-08 03:03:57 -0400

    smalllllll...

--- app/src/main/java/com/juliachihata/co_pilot/MainActivity.java
+++ app/src/main/java/com/juliachihata/co_pilot/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         but2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent int2= new Intent(MainActivity.this, DifficultyActivity.class);
+                Intent int2= new Intent(MainActivity.this, AwarenessActivity.class);
                 startActivity(int2);
             }
 
