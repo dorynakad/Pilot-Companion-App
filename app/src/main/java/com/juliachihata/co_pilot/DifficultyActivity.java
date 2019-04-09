4d1a015a0ec5fc028d00eeee6dd2f6832a65402e
Julia <juliachihata@gmail.com>
2019-04-09 10:52:04 -0400

    fix 0.08

--- app/src/main/java/com/juliachihata/co_pilot/DifficultyActivity.java
+++ app/src/main/java/com/juliachihata/co_pilot/DifficultyActivity.java
@@ -10,6 +10,7 @@ import android.support.annotation.RequiresApi;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -39,6 +40,7 @@ public class DifficultyActivity extends AppCompatActivity {
     int difint;
     AwarenessActivity awarenessActivity = new AwarenessActivity();
     View view;
+
 
 
     public void updateeft(int eft){
